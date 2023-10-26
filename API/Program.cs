9c2e43234cdd68198253f87b661cfc37e2d26747
Daniel12082 <137439716+Daniel12082@users.noreply.github.com>
2023-10-26 10:51:38 -0500

    feat: :sparkles: Update

--- API/Program.cs
+++ API/Program.cs
@@ -3,36 +3,34 @@ using API.Extension;
 using AspNetCoreRateLimit;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.ConfigureRateLimiting();
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
 builder.Services.ConfigureCors();
 builder.Services.AddAplicationServices();
-builder.Services.ConfigureRateLimiting();
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-builder.Services.AddDbContext<TiendaCampusContext>(options =>
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppNotiContext>(options =>
 {
     string connectionString = builder.Configuration.GetConnectionString("MySqlConex");
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
-builder.Services.AddAplicationServices();
 var app = builder.Build();
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 app.UseCors("CorsPolicy");
-app.UseIpRateLimiting();
 app.UseHttpsRedirection();
+app.UseIpRateLimiting();
 app.UseAuthorization();
 app.MapControllers();
 
